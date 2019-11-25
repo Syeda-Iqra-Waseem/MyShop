5854702327fd94d5e30229dbd099cc421f7d30e6
Syeda-Iqra-Waseem <39098383+Syeda-Iqra-Waseem@users.noreply.github.com>
2019-11-26 01:12:59 +0500

    Implemented Generic Repository

--- MyShop/MySop.Core/Models/ProductCategory.cs
+++ MyShop/MySop.Core/Models/ProductCategory.cs
@@ -6,16 +6,13 @@ using System.Threading.Tasks;
 
 namespace MySop.Core.Models
 {
-   public class ProductCategory
+   public class ProductCategory:BaseEntity
     {
-        public string Id { get; set; }
+        
         public string Category { get; set; }
 
 
-        public ProductCategory()
-        {
-            this.Id = Guid.NewGuid().ToString();
-        }
+       
 
     }
 }
